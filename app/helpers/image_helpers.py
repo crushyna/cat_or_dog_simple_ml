73797715db5a72522d083410c86e098c2af9aacb
Radek M <46869083+crushyna@users.noreply.github.com>
2021-02-18 16:45:28 +0100

    added test_image_helpers.py, fixed stylesheet imports

--- app/helpers/image_helpers.py
+++ app/helpers/image_helpers.py
@@ -11,9 +11,19 @@ class ImageHelpers:
 
     @staticmethod
     def create_thumbnail(image_source_path: str, image_destination_path: str) -> str:
+        """
+        Create smaller image of one provided by user.
+        This image will be shown on results page.
+
+        :param image_source_path: str
+        :param image_destination_path: str
+        :return: image_destination_path: str
+        """
         try:
             image = Image.open(image_source_path)
             image.thumbnail((400, 400))
             image.save(image_destination_path)
         except OSError as er:
-            render_template("index.html", error=er)
+            return f"Error: {er}"
+
+        return image_destination_path
---
+++ tests/test_image_helpers.py
@@ -0,0 +1,18 @@
+import pytest
+
+from PIL import Image
+from helpers.image_helpers import ImageHelpers
+from helpers.string_helpers import StringHelpers
+
+
+class TestImageHelpers:
+    def test_create_thumbnail(self):
+        test_filename = 'static/resources/temp/cat_example.jpg'
+        test_filename_thumbnail = f"static/resources/temp/cat_example_thumbnail.jpg"
+        ImageHelpers.create_thumbnail(test_filename, test_filename_thumbnail)
+        test_image = Image.open(test_filename_thumbnail)
+        assert test_image.size[0] <= 400
+        assert test_image.size[1] <= 400
+
+    def test_create_image_for_ml(self):
+        pass
