cd1d445d840af7d4b615a8b74bcb9ba50230beb6
Radek M <46869083+crushyna@users.noreply.github.com>
2021-02-24 16:12:52 +0100

    file cleanup updated, image for unknown recognition added, error handling added, page titles added, pylint fixes

--- app/main.py
+++ app/main.py
@@ -4,17 +4,19 @@ This application requires working Azure Function that works as ML Server.
 """
 import logging
 import os
-import requests
-from flask import Flask, render_template, request, redirect, session, abort, url_for
+from flask import Flask, render_template, request, redirect, session, url_for, abort
 from models.ml_response import MLResponseClass
 from helpers.file_cleanup import FileCleanup
 from helpers.string_helpers import StringHelpers
 from helpers.image_helpers import ImageHelpers
 
 logging.basicConfig(filename=os.path.join('logs', 'application.log'), level=logging.DEBUG,
-                    format='%(asctime)s.%(msecs)03d : %(levelname)s : %(message)s', datefmt='%Y/%m/%d %H:%M:%S')
+                    format='%(asctime)s.%(msecs)03d : %(levelname)s : %(message)s',
+                    datefmt='%Y/%m/%d %H:%M:%S')
 
 UPLOAD_FOLDER = 'static/resources/temp'
+LOGS_FOLDER = 'logs'
+
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
@@ -24,7 +26,18 @@ APP = Flask(__name__)
 APP.secret_key = b'crushyna'
 APP.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 APP.config['MAX_CONTENT_LENGTH'] = 5 * 1024 * 1024
-APP.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png']
+APP.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', 'jpeg']
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+
+def allowed_file(filename):
+    """
+    Function for checking, if uploaded file has correct extension.
+    :param filename:
+    """
+    logging.info(f"Checking {filename} extension...")
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @APP.route("/hello")
@@ -33,7 +46,9 @@ def hello():
     Test endpoint for application.
     :return: string
     """
-    return "Hello World from Flask in a uWSGI Nginx Docker container with Python 3.9.1 (from the example template)"
+    session['page_title'] = 'Hello!'
+    return "Hello World from Flask in a uWSGI Nginx Docker " \
+           "container with Python(from the example template)"
 
 
 @APP.route("/")
@@ -41,6 +56,8 @@ def main():
     """
     Main route for the application. All wrong addresses are redirected here as well.
     """
+    FileCleanup.file_cleanup(LOGS_FOLDER)
+    session['page_title'] = 'Start'
     logging.info("Rendering welcome template.")
     return render_template("index.html")
 
@@ -50,22 +67,9 @@ def results():
     """
     Page displaying results of the Machine Learning
     """
+    session['page_title'] = 'Results'
     FileCleanup.file_cleanup(UPLOAD_FOLDER)
     return render_template("results.html")
-
-
-def recognize_animal(param: str) -> str:
-    """
-    Temporary function for ML engine.
-    :param param: str
-    """
-    url = f"{os.getenv('ML_SERVER')}"
-    response = requests.request("GET", url)
-
-    # animals = ['cat', 'dog']
-    # answer = random.choice(animals)
-
-    return response.json()['message']
 
 
 @APP.route("/", methods=['POST'])
@@ -78,11 +82,16 @@ def upload_file():
     Finally, function redirects to results page.
     :return: result page
     """
+    session['filetype_ok'] = True  # assert as default value for modal message
     uploaded_file = request.files['image_file']
     if uploaded_file.filename != '':
-        file_ext = os.path.splitext(uploaded_file.filename)[1]
-        if file_ext not in APP.config['UPLOAD_EXTENSIONS']:
-            abort(400)  # TODO: add redirection to web page or modal with info
+        if not allowed_file(uploaded_file.filename):
+            logging.info(f"Wrong file extension!")
+            session['filetype_ok'] = False
+            abort(502)
+
+        logging.info(f"Correct file extension. Proceeding.")
+        session['filetype_ok'] = True
 
         temp_filename = StringHelpers.generate_random_string(8)
         temp_filename_thumbnail = f"{temp_filename}_thumbnail.jpg"
@@ -102,15 +111,44 @@ def upload_file():
 
         session['temp_filename'] = temp_filename
         session['temp_filename_thumbnail'] = temp_filename_thumbnail
+
+    else:
+        session['filetype_ok'] = False
+        abort(502)
+
     return redirect(url_for('results'))
 
 
-# TODO: Add redirect for 400 BAD_REQUEST
-
 # ONLY Error handling below #
 @APP.errorhandler(404)
-def page_not_found(e):
-    return render_template("index.html")
+def page_not_found(error):
+    """
+    Page not found handler.
+    Redirects to starting page.
+    :return: starting page
+    """
+    return render_template("index.html", error=error)
+
+
+@APP.errorhandler(413)
+def payload_too_large(error):
+    """
+    Payload too large handler.
+    Redirects to error page with proper message.
+    :return: error page with custom message
+    """
+    return render_template("error.html", error_message=f"File too large! Try again. {error}"), 413
+
+
+@APP.errorhandler(502)
+def bad_gateway(error):
+    """
+    Bad gateway handler.
+    Occurs when file uploaded by user has improper type.
+    Redirects to error page with proper message.
+    :return: error page with custom message
+    """
+    return render_template("error.html", error_message=f"Wrong file type or file is missing. Images only! {error}"), 502
 
 
 if __name__ == "__main__":
